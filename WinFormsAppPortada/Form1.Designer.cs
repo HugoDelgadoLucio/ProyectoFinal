2b9795fadb45985822c4de5860ea957365c14b0d
Nestor Gael <nesgami@hotmail.com>
2024-12-14 18:03:45 -0600

    Añadido proyecto WinFormsAppPortada al repositorio

---
+++ WinFormsAppPortada/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace WinFormsAppPortada
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            Integrantes = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            pictureBox2 = new PictureBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources.Taco_mascot1;
+            pictureBox1.Location = new Point(27, 9);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(247, 277);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Integrantes
+            // 
+            Integrantes.AutoSize = true;
+            Integrantes.BackColor = Color.Transparent;
+            Integrantes.Font = new Font("Mexicana Hollow", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Integrantes.ForeColor = Color.Maroon;
+            Integrantes.Location = new Point(231, 236);
+            Integrantes.Name = "Integrantes";
+            Integrantes.Size = new Size(279, 35);
+            Integrantes.TabIndex = 1;
+            Integrantes.Text = "Integrantes:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Mexicana", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.IndianRed;
+            label1.Location = new Point(167, 297);
+            label1.Name = "label1";
+            label1.Size = new Size(430, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Hugo Ernesto Delgado Lucio";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Mexicana", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.IndianRed;
+            label2.Location = new Point(167, 333);
+            label2.Name = "label2";
+            label2.Size = new Size(418, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Angel Gael Andrade Ovalle";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Mexicana", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.IndianRed;
+            label3.Location = new Point(216, 369);
+            label3.Name = "label3";
+            label3.Size = new Size(351, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Jetzan Azael Esquivel ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Mexicana", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.IndianRed;
+            label4.Location = new Point(113, 409);
+            label4.Name = "label4";
+            label4.Size = new Size(537, 25);
+            label4.TabIndex = 5;
+            label4.Text = "Alexander Vladimir Rangel Gudino";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Mexicana", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.IndianRed;
+            label5.Location = new Point(167, 444);
+            label5.Name = "label5";
+            label5.Size = new Size(454, 25);
+            label5.TabIndex = 6;
+            label5.Text = "Nestor Gael MartInez Ibarra";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("El Rio Lobo", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.FromArgb(0, 64, 0);
+            label6.Location = new Point(220, 9);
+            label6.Name = "label6";
+            label6.Size = new Size(661, 42);
+            label6.TabIndex = 7;
+            label6.Text = "Comida Mexicana Doña Dominga";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("El Rio Lobo", 13.8F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.Brown;
+            label7.Location = new Point(231, 52);
+            label7.Name = "label7";
+            label7.Size = new Size(235, 22);
+            label7.TabIndex = 8;
+            label7.Text = "De Mexico A Tu PLato";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.Font = new Font("Mexicana Hollow", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.Maroon;
+            label8.Location = new Point(893, 227);
+            label8.Name = "label8";
+            label8.Size = new Size(215, 35);
+            label8.TabIndex = 9;
+            label8.Text = "Profesor:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.Transparent;
+            label9.Font = new Font("Mexicana", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.IndianRed;
+            label9.Location = new Point(786, 276);
+            label9.Name = "label9";
+            label9.Size = new Size(408, 25);
+            label9.TabIndex = 10;
+            label9.Text = "GEORGINA SALAZAR PARTIDA";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.Transparent;
+            label10.Font = new Font("Mexicana Hollow", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.ForeColor = Color.Maroon;
+            label10.Location = new Point(893, 323);
+            label10.Name = "label10";
+            label10.Size = new Size(195, 35);
+            label10.TabIndex = 11;
+            label10.Text = "Carrera:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.Transparent;
+            label11.Font = new Font("Mexicana", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label11.ForeColor = Color.IndianRed;
+            label11.Location = new Point(762, 369);
+            label11.Name = "label11";
+            label11.Size = new Size(485, 25);
+            label11.TabIndex = 12;
+            label11.Text = "Ing. Sistemas Computacionales";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.BackColor = Color.Transparent;
+            label12.Font = new Font("Mexicana Hollow", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label12.ForeColor = Color.Maroon;
+            label12.Location = new Point(893, 423);
+            label12.Name = "label12";
+            label12.Size = new Size(190, 35);
+            label12.TabIndex = 13;
+            label12.Text = "Materia:";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.BackColor = Color.Transparent;
+            label13.Font = new Font("Mexicana", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label13.ForeColor = Color.IndianRed;
+            label13.Location = new Point(882, 467);
+            label13.Name = "label13";
+            label13.Size = new Size(238, 25);
+            label13.TabIndex = 14;
+            label13.Text = "Programacion ";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.BackColor = Color.Transparent;
+            label14.Font = new Font("Tacos de Tijuana", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label14.ForeColor = Color.FromArgb(0, 64, 0);
+            label14.Location = new Point(27, 568);
+            label14.Name = "label14";
+            label14.Size = new Size(212, 37);
+            label14.TabIndex = 15;
+            label14.Text = "05/12/2024";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Image = Properties.Resources.UAA__1_;
+            pictureBox2.Location = new Point(1073, -65);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(241, 224);
+            pictureBox2.TabIndex = 16;
+            pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Maroon;
+            button1.Font = new Font("Mexicana Hollow", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(616, 545);
+            button1.Name = "button1";
+            button1.Size = new Size(168, 60);
+            button1.TabIndex = 17;
+            button1.Text = "INGRESAR";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 255, 192);
+            BackgroundImage = Properties.Resources.Imagen_de_WhatsApp_2024_12_05_a_las_18_13_19_8c457d6a;
+            ClientSize = new Size(1340, 647);
+            Controls.Add(button1);
+            Controls.Add(pictureBox2);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(Integrantes);
+            Controls.Add(pictureBox1);
+            ForeColor = Color.Transparent;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label Integrantes;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private PictureBox pictureBox2;
+        private Button button1;
+    }
+}
